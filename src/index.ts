2e17ce644f29509c058182ac7f9915a8f89d05af
Silvio Dayube <scarige@gmail.com>
2022-11-27 00:40:48 -0300

    Add fetching route prototype

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,42 @@
 import express from 'express';
+import axios from 'axios';
 
 const app = express();
 
-app.get('/', (req, res) => {
-  res.json({ message: 'Hello World!' });
+interface Film {
+  title: string;
+  movie_banner: string;
+  description: string;
+  director: string;
+  producer: string;
+}
+
+app.post('/films', (req, res) => {
+  axios
+    .get('https://ghibliapi.herokuapp.com/films', {
+      headers: { 'accept-encoding': null },
+    })
+    .then((response) => {
+      const filmList = response.data.map((film: Film) => {
+        const {
+          title,
+          movie_banner: banner,
+          description,
+          director,
+          producer,
+        } = film;
+
+        return {
+          title,
+          banner,
+          description,
+          director,
+          producer,
+        };
+      });
+
+      res.json(filmList);
+    });
 });
 
 app.listen(3333, () => {
