ec76a02f0f7b28adab8c720b973e83a20aaaf431
Silvio Dayube <scarige@gmail.com>
2022-11-26 23:36:21 -0300

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,11 @@
+const express = require('express');
+
+const app = express();
+
+app.get('/', (req, res) => {
+  res.json({ message: 'Hello World!' });
+});
+
+app.listen(3333, () => {
+  console.log('Server listening on port 3333');
+});
